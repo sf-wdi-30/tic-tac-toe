f899357f69d06b25c7ade6be7884f5934a918e20
danieloh91 <daniel.oh.91@gmail.com>
2016-07-01 16:30:57 -0700

    link css to html

--- js/app.js
+++ js/app.js
@@ -3,4 +3,25 @@ $(document).ready(function() {
   // all code to manipulate the DOM
   // goes inside this function
 
+function xWins {
+  if x = 
+}
+
+  function currentSquare() {
+    return $("tr").find("td").eq(kyrel.x);
+  }
+
+
+  function draw() {
+    $(".instructions").append("<div>draw</div>");
+    currentSquare().html('<div class="dot"></div>');
+    currentSquare().find('.dot').css('background', kyrel.color).addClass('dot-'+kyrel.color);
+  }
+
+  .on("click", function(e)) {
+    $().append();
+  }
+
+
+
 });
