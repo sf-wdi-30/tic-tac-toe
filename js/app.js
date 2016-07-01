4eb00ab84e288fdd114d69fabec7903bf58bcd1c
tbduong <tiffaniduong@gmail.com>
2016-07-01 16:50:23 -0700

    input click test

--- js/app.js
+++ js/app.js
@@ -2,5 +2,14 @@
 $(document).ready(function() {
   // all code to manipulate the DOM
   // goes inside this function
+$(".col-md-4").on("click",function(e)){
+  $(this).css("background", "blue");
+});
+
+
+
+
 
 });
+
+console.log("hi")
