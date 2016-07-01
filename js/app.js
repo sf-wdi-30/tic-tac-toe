e50825dc5362282fa1516979fe3af3e78a4b0df8
SwittS <switt.srikulacheep@gmail.com>
2016-07-01 16:49:55 -0700

    complete full box and testing box click

--- js/app.js
+++ js/app.js
@@ -1,5 +1,12 @@
+console.log("frommmmmmmmmm Chicago");
+//alert("I am an alert box!");
 // wait for the DOM to finish loading
 $(document).ready(function() {
+
+    $('.col-md-4 box').on("click", function handleClick(e) {
+      $(this).alert("It works!");
+    });
+
   // all code to manipulate the DOM
   // goes inside this function
 
