22eb86e1678e1f819cb9c245b64cd201739f84f0
danieloh91 <daniel.oh.91@gmail.com>
2016-07-01 16:45:28 -0700

    link js and jquery

--- js/app.js
+++ js/app.js
@@ -1,26 +1,32 @@
 // wait for the DOM to finish loading
+  console.log("this is a sanity check");
 $(document).ready(function() {
   // all code to manipulate the DOM
   // goes inside this function
 
-function xWins {
-  if x = 
-}
-
-  function currentSquare() {
-    return $("tr").find("td").eq(kyrel.x);
-  }
 
 
-  function draw() {
-    $(".instructions").append("<div>draw</div>");
-    currentSquare().html('<div class="dot"></div>');
-    currentSquare().find('.dot').css('background', kyrel.color).addClass('dot-'+kyrel.color);
-  }
+  console.log("Let's get coding!");
 
-  .on("click", function(e)) {
-    $().append();
-  }
+// var x = console.log('sanity check');
+// function xWins {
+//   if x =
+// }
+//
+//   function currentSquare() {
+//     return $("tr").find("td").eq(kyrel.x);
+//   }
+//
+//
+//   function draw() {
+//     $(".instructions").append("<div>draw</div>");
+//     currentSquare().html('<div class="dot"></div>');
+//     currentSquare().find('.dot').css('background', kyrel.color).addClass('dot-'+kyrel.color);
+//   }
+//
+//   .on("click", function(e)) {
+//     $().append();
+//   }
 
 
 
