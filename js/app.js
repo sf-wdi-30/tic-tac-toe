63de036af7ee3b30df83fa3a1c4a2c9d4215445b
jamestothe <jamesdelacruz@gmail.com>
2016-07-01 16:50:19 -0700

    added a few jQuery tests and changed background to green

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
 // wait for the DOM to finish loading
 alert("Sanity Check!");
+
 $(document).ready(function() {
   // all code to manipulate the DOM
   // goes inside this function
-
+    $("h1").on("click", function testClick(event){
+        alert("You clicked me!");
+    });
 });
