381be0fe0b2353858fa95d352b8dca5390888ec9
SwittS <switt.srikulacheep@gmail.com>
2016-07-02 14:36:41 -0700

    optimize function to include conditional statement and allow for alternate moves after click

--- js/app.js
+++ js/app.js
@@ -5,16 +5,35 @@ $(document).ready(function() {
 
   // all code to manipulate the DOM
 
+var board = 0; //default value of board set to zero
 
-      $('.box').on("click", function handleClick(e) { // how to click a specific box
-        var whichBox = $(this).html();
-        alert("You just picked" + whichBox);
+      $('.row').find('.box').click( // optimized click function to ensure alternating "X" and "O" after click
+        function(){
+          if(board%2 === 0){ // if board % 2 === 0 draw "X" --> true
+            $(this).text("X");
+          } else{ // if board % 2 === 0 draw "O"
+            $(this).text("O");
+          }
+          board++; // board + 1
       });
 
-      $('.box').on("click", function drawX(){ // how to insert x after clicking
-        $(this).append("x");
-      })
 
+
+      //$('.box').on("click", function handleClick(e) { // how to click a specific box
+        //var whichBox = $(this).html();
+        //alert(player1 + " " + "picked" + whichBox);
+      //});
+
+      //$('.box').on("click", function drawX(){ // how to insert x after clicking
+        //$(this).append("x");
+      //})
+
+  // how to alternate to player2
+
+
+// how to prevent the player from choosing a previously claimed box
+      //preventDefault();
+// how to reset the game and clear the board
       $('.btn').on("click", function handleClick(e){ // how to reset the game
         alert("Reset!");
       });
