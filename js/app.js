5058f826d256a195b70da32e13085ecbd5f476ee
YOLANDA RODRIGUEZ <Array415@gmail.com>
2016-07-01 22:50:12 -0700

    Adding Win conditions

--- js/app.js
+++ js/app.js
@@ -4,3 +4,28 @@ $(document).ready(function() {
   // goes inside this function
 
 });
+
+var turn = true;
+
+$('.box').one('click', function(e){
+
+    if(turn === true){
+      $(this).css('background-image', 'url("https://www.petfinder.com/wp-content/uploads/2012/11/122163343-conditioning-dog-loud-noises-632x475.jpg")');
+      $(this).text('X');
+      didIWin();
+      turn = false;
+    }
+    else {
+      $(this).css('background-image', 'url("http://www.petdrugsonline.co.uk/images/page-headers/cats-master-header")');
+      $(this).text('O');
+      turn = true;
+
+  }
+});
+
+
+function didIWin() {
+if($('.topLeft').text() === "X" && $('.topMiddle').text() === "X" && $('.topRight').text() === "X") {
+  alert('WOW');
+}
+}
