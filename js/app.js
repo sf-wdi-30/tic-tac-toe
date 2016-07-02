62bc28c6f537e8bd7c73fc1139334e6803078004
jamestothe <jamesdelacruz@gmail.com>
2016-07-02 13:15:53 -0700

    added alert to j tells us which box is being clicked, added unique classes to each ttt box html

--- js/app.js
+++ js/app.js
@@ -4,7 +4,12 @@ alert("Sanity Check!");
 $(document).ready(function() {
   // all code to manipulate the DOM
   // goes inside this function
-    $("h1").on("click", function testClick(event){
-        alert("You clicked me!");
+
+    $(".box").on("click", function testClick(event){
+      var whatBox = $(this).html(); // changed so that alert tells which box is being clicked
+        alert("You clicked " + whatBox);
+    });
+    $(".box").on("click", function insertX(e){ //adds x to each box
+      $(this).append("x");
     });
 });
