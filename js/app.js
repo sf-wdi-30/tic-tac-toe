7a06fb690f434c0a070fd78cd6596e7114af755f
jamestothe <jamesdelacruz@gmail.com>
2016-07-03 15:24:47 -0700

    game is working now working on adding reset button

--- js/app.js
+++ js/app.js
@@ -3,67 +3,77 @@ alert("Sanity Check!");
 $(document).ready(function() {
 //   // all code to manipulate the DOM
 //   // goes inside this function
-var turnCounter = 0; //maximum of 9 turns
-
+  var turnCounter = 0; //maximum of 9 turns
 
   $(".box").on("click", function checkBox(event){
-      if ($(this).hasClass("took")){// ask TAs
+      if ($(this).hasClass("took")){ // conditional statement that checks if square is taken
         alert("Can't move here");
       }
-      else{
-          if (turnCounter % 2 === 0){ //adds value of x or o to each box and changes text
+      else{ // else will execute if an open square is selected
+          if (turnCounter % 2 === 0){ // nested conditional statement that adds color and value to squares
             $(this).val("x");
             $(this).text("x");
-            $(this).addClass("took");
             $(this).css('background-color', ("blue"));
           }
           else if (turnCounter % 2 === 1){
             $(this).val("o");
             $(this).text("o");
-            $(this).addClass("took");
             $(this).css('background-color', ("red"));
           }
-      turnCounter++;    
+      $(this).addClass("took");
+      turnCounter++;
       }
-
-      if (($(".1stbox").val() && $(".2ndbox").val() && $(".3rdbox").val()) === "o"){ // ask TAs
+// checking for winner 
+      if ($(".1stbox").val() === "o" && $(".2ndbox").val() === "o" && $(".3rdbox").val() === "o"){ // ask ryan
         alert("o wins!");
       }
-      else if ($(".4thbox").val() && $(".5thbox").val() && $(".6thbox").val() === "o"){
+      else if ($(".4thbox").val() === "o" && $(".5thbox").val() === "o" && $(".6thbox").val() === "o"){
         alert("o wins!");
       }
-      else if ($(".7thbox").val() && $(".8thbox").val() && $(".9thbox").val() === "o"){
+      else if ($(".7thbox").val() === "o" && $(".8thbox").val() === "o" && $(".9thbox").val() === "o"){
         alert("o wins!");
       }
-      else if ($(".1stbox").val() && $(".4thbox").val() && $(".7thbox").val() === "o"){
+      else if ($(".1stbox").val() === "o" && $(".4thbox").val() === "o" && $(".7thbox").val() === "o"){
         alert("o wins!");
       }
-      else if ($(".2ndbox").val() && $(".5thbox").val() && $(".8thbox").val() === "o"){
+      else if ($(".2ndbox").val() === "o" && $(".5thbox").val() === "o" && $(".8thbox").val() === "o"){
         alert("o wins!");
       }
-      else if ($(".3rdbox").val() && $(".6thbox").val() && $(".9thbox").val() === "o"){
+      else if ($(".3rdbox").val() === "o" && $(".6thbox").val() === "o" && $(".9thbox").val() === "o"){
         alert("o wins!");
       }
-      else if ($(".1stbox").val() && $(".5thbox").val() && $(".9thbox").val() === "o"){
+      else if ($(".1stbox").val() === "o" && $(".5thbox").val() === "o" && $(".9thbox").val() === "o"){
         alert("o wins!");
       }
-      else if ($(".3rdbox").val() && $(".5thbox").val() && $(".7thbox").val() === "o"){
+      else if ($(".3rdbox").val() === "o" && $(".5thbox").val() === "o" && $(".7thbox").val() === "o"){
         alert("o wins!");
       }
-
+      else if ($(".1stbox").val() === "x" && $(".2ndbox").val() === "x" && $(".3rdbox").val() === "x"){ // ask ryan
+        alert("x wins!");
+      }
+      else if ($(".4thbox").val() === "x" && $(".5thbox").val() === "x" && $(".6thbox").val() === "x"){
+        alert("x wins!");
+      }
+      else if ($(".7thbox").val() === "x" && $(".8thbox").val() === "x" && $(".9thbox").val() === "x"){
+        alert("x wins!");
+      }
+      else if ($(".1stbox").val() === "x" && $(".4thbox").val() === "x" && $(".7thbox").val() === "x"){
+        alert("x wins!");
+      }
+      else if ($(".2ndbox").val() === "x" && $(".5thbox").val() === "x" && $(".8thbox").val() === "x"){
+        alert("x wins!");
+      }
+      else if ($(".3rdbox").val() === "x" && $(".6thbox").val() === "x" && $(".9thbox").val() === "x"){
+        alert("x wins!");
+      }
+      else if ($(".1stbox").val() === "x" && $(".5thbox").val() === "x" && $(".9thbox").val() === "x"){
+        alert("x wins!");
+      }
+      else if ($(".3rdbox").val() === "x" && $(".5thbox").val() === "x" && $(".7thbox").val() === "x"){
+        alert("x wins!");
+      }
+      else if (turnCounter === 9){
+        alert("Draw!");
+      }
     });
 });
-
-
-      //
-      // $(".box").on("click", function changeColor(event){ //changesColor of each box
-      //   if ($(this).val() === "x"){ //changes color to blue if box value is x, red if o
-      //     $(this).css('background-color', ("blue"));
-      //   }
-      //   else {
-      //     $(this).css('background-color', ("red"));
-      //   }
-      // });
-
-
-      //checking for wins
