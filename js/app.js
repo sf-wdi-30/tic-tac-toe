71405e0f5b33c290c71aa50cee7963540c555cc0
SwittS <switt.srikulacheep@gmail.com>
2016-07-03 14:47:51 -0700

    add a hasClass and addClass to disallow a user from picking that box

--- js/app.js
+++ js/app.js
@@ -7,54 +7,63 @@ $(document).ready(function() {
 
 var board = 0; //default value of board set to zero
 
-      $('.row').find('.box').click( // optimized click function to ensure alternating "X" and "O" after click
-        function(){
-          if(board%2 === 0){ // if board % 2 === 0 draw "X" --> true
-            $(this).text("X");
-            $(this).val("X");
-          } else{ // if board % 2 === 0 draw "O"
-            $(this).text("O");
-            $(this).val("O");
-          }
-          board++; // board + 1
-        });
 
-$('.box').click(function fillbox(){
-  if($(this).val() === "X"){
-    $(this).css('background-color', ("blue"));
-  } else {
-    $(this).css('background-color', ("green"));
+//      $('.row').find('.box').click( // optimized click function to ensure alternating "X" and "O" after click
+  //      function (){
+    //      if(board%2 === 0){ // if board % 2 === 0 draw "X" --> true
+      //      $(this).text("X"); // inputs text into box when clicked
+      //      $(this).val("X");
+      //      $(this).addClass("claimed"); // inputs value
+      //    } else{ // if board % 2 === 0 draw "O"
+      //      $(this).text("O");
+      //      $(this).val("O");
+      //      $(this).addClass("claimed");
+    //      }
+    //      board++; // board + 1
+    //    });
+
+$('.box').click(function (){ // if box has value, don't allow for input
+  if ($(this).hasClass("claimed")){ // if box has been claimed by the "claimed" class then alert user
+    alert("box has already been claimed");
   }
+  else {
+    if(board%2 === 0){ // if board % 2 === 0 draw "X" --> true
+      $(this).text("X"); // inputs text into box when clicked
+      $(this).val("X");
+      $(this).addClass("claimed"); // adds claimed class after click
+      $(this).css('background-color', ("blue")); // inputs value
+    }
+    else{
+    // if board % 2 === 0 draw "O"
+      $(this).text("O");
+      $(this).val("O");
+      $(this).addClass("claimed");
+      $(this).css('background-color', ("green"));
+    }
+        board++; // board + 1
+
+  }
+
 });
-    //  $('.box').click( function fillBox(){
-      //      if($(this).text === "X"); {
-      //        $(this).css('background-color', player1);
-          //  } else {
-      //       $(this).css('background-color', player2);
 
-      //   }
-
-
-      //      $(this).css('background-color', player2)
-      //  });
-
-
-
-
-
-      //$('.box').on("click", function handleClick(e) { // how to click a specific box
-        //var whichBox = $(this).html();
-        //alert(player1 + " " + "picked" + whichBox);
-      //});
-
-      //$('.box').on("click", function drawX(){ // how to insert x after clicking
-        //$(this).append("x");
-      //})
-
-  // how to alternate to player2
-
+      $('.box').on("click", function handleClick(e) { // how to click a specific box
+        var whichBox = $(this).html();
+        alert("You" + " " + "picked" + " " + whichBox);
+      });
 
 // how to prevent the player from choosing a previously claimed box
+//if a box value is empty you may pick a box, if it's value is x or o then you preventDefault?
+//$('.box').click(function isBoxClaimed(e){
+//  if($(this).val() === "X" || "O"){
+//    alert("box has already been claimed");
+  // }
+// }
+
+//$(".box").click(function clicky(e) {
+  // if ($(this).val() === "X" || "O") {
+  //   alert("pick another box");
+   //}
+//});
 
 // how to reset the game and clear the board
       $('.btn').on("click", function handleClick(e){ // how to reset the game
