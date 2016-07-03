5b871f3942dcb5712e530ad5a70ac5d2e166cbd5
YOLANDA RODRIGUEZ <Array415@gmail.com>
2016-07-03 13:47:19 -0700

    trying something new / adding final page tweaks

--- js/app.js
+++ js/app.js
@@ -12,8 +12,9 @@ $(document).ready(function(e) {
     if(turn === true){
       $(this).css('background-image', 'url("https://www.petfinder.com/wp-content/uploads/2012/11/122163343-conditioning-dog-loud-noises-632x475.jpg")');
       $(this).text('X');
-      didXWin();
+      didXWin();                 // Player X
       draw();
+      declareWinner();
       turn = false;
       finalCountDown += 1;    // do ~ do ~ do ~ dooo ~ do ~ do ~ do ~ do ~ dooo
 
@@ -21,36 +22,57 @@ $(document).ready(function(e) {
 
     else {
 
-      $(this).css('background-image', 'url("http://www.petdrugsonline.co.uk/images/page-headers/cats-master-header")');
+      $(this).css('background-image', 'url("https://pixabay.com/static/uploads/photo/2014/03/29/09/17/cat-300572_960_720.jpg")');
       $(this).text('O');
-      didOWin();
+      didOWin();                // Player O
       draw();
+      declareWinner();
       turn = true;
       finalCountDown += 1;     // do ~ do ~ do ~ dooo ~ do ~ do ~ do ~ do ~ dooo
     }
   });
 
+  $('.box').hover(
+    function() {
+      $(this).addClass('hover');     // Changes colour on hover.
+    },
+    function(){
+      $(this).removeClass('hover');
+    }
+  );
+
+ function declareWinner(){
+   if(xWins === true){
+     $('h3').text('Dog Wins!');
+   }
+   else if (oWins === true){
+     $('h3').text('Cat Wins!');
+   }
+ }
+
 });
 
 function didXWin() {
-  if( $('.topLeft').text() === "X"    &&  $('.topMiddle').text() === "X"    &&  $('.topRight').text() === "X"     ||    // broke them into 2 because i wanted my win messages to say "X wins Horizontally etc."
-      $('.middleLeft').text() === "X" &&  $('.middleMiddle').text() === "X" &&  $('.middleRight').text() === "X"  ||    // X wins X  - i want to refactor this have to think harder.
+  if( $('.topLeft').text() === "X"    &&  $('.topMiddle').text() === "X"     &&  $('.topRight').text() === "X"     ||    // broke them into 2 because i wanted my win messages to say "X wins Horizontally etc."
+      $('.middleLeft').text() === "X" &&  $('.middleMiddle').text() === "X"  &&  $('.middleRight').text() === "X"  ||    // X wins X  - i want to refactor this have to think harder.
       $('.bottomLeft').text() === "X" &&  $('.bottomMiddle').text() === "X"  &&  $('.bottomRight').text() === "X"  ||
-      $('.topLeft').text() === "X"    &&  $('.middleMiddle').text() === "X" &&  $('.bottomRight').text() === "X") {    // X diagonal
+      $('.topLeft').text() === "X"    &&  $('.middleMiddle').text() === "X"  &&  $('.bottomRight').text() === "X" ) {    // X diagonal
 
         console.log('boo');
         xWins = true;
+
       }
 
                                                                                                                                                                                                                                                                                            //    O
   else if( $('.topLeft').text() === "X"   &&  $('.middleLeft').text()   === "X"  &&  $('.bottomLeft').text() === "X"   ||
-           $('.topMiddle').text() === "X" &&  $('.middleMiddle').text() === "X"  &&  $('.bottomMiddle').text() === "X"  ||   // X wins Y
+           $('.topMiddle').text() === "X" &&  $('.middleMiddle').text() === "X"  &&  $('.bottomMiddle').text() === "X" ||   // X wins Y
            $('.topRight').text() === "X"  &&  $('.middleRight').text()  === "X"  &&  $('.bottomRight').text() === "X"  ||                                                                                                                                                                  //    O
-           $('.topRight').text() === "X"  &&  $('.middleMiddle').text()  === "X"  &&  $('.bottomLeft').text() === "X")  {    // X diagonal
+           $('.topRight').text() === "X"  &&  $('.middleMiddle').text()  === "X" &&  $('.bottomLeft').text() === "X" ) {    // X diagonal
 
 
-           console.log('boo');
-           xWins = true;
+             console.log('boo');
+             xWins = true;
+
            }
 }
 
@@ -59,18 +81,21 @@ function didOWin() {
       $('.middleLeft').text() === "O" &&  $('.middleMiddle').text() === "O" &&  $('.middleRight').text() === "O"   ||    // O wins X  - i want to refactor this have to think harder.
       $('.bottomLeft').text() === "O" &&  $('.bottomMiddle').text() === "O"  &&  $('.bottomRight').text() === "O"   ||
       $('.topLeft').text() === "O"    &&  $('.middleMiddle').text() === "O" &&  $('.bottomRight').text() === "O")  {    // O diagonal
-      console.log('boo');
-      oWins = true;
+
+        console.log('boo');
+        oWins = true;
+
    }
 
                                                                                                                                                                                                                                                                                            //    O
   else if( $('.topLeft').text() === "O"   &&  $('.middleLeft').text()   === "O"  &&  $('.bottomLeft').text() === "O"   ||  // O wins Y
            $('.topMiddle').text() === "O" &&  $('.middleMiddle').text() === "O"  &&  $('.bottomMiddle').text() === "O"  ||
-           $('.topRight').text() === "O"  &&  $('.middleRight').text()  === "O"  &&  $('.bottomRight').text() === "O"  ||                                                                                                                                                                  //    O
+           $('.topRight').text() === "O"  &&  $('.middleRight').text()  === "O"  &&  $('.bottomRight').text() === "O"  ||     // super ugly                                                                                                                                                             //    O
            $('.topRight').text() === "O"  &&  $('.middleMiddle').text()  === "O"  &&  $('.bottomLeft').text() === "O")  {    // O diagonal
 
            console.log('boo');
            oWins = true;
+
   }
 
 
@@ -79,5 +104,39 @@ function didOWin() {
 function draw(){
   if (finalCountDown === 8 && xWins === false && oWins === false){
     alert('DRAW');
+  }
+}
+
+var $board;
+var a = [["X", "X", "X"], ["O", "O", "O"]];
+
+ function boo() {
+   $board = [ [ $('.topLeft').text(), $('.topMiddle').text(), $('.topRight').text()] ,
+               [ $('.middleLeft').text(), $('.middleMiddle').text(), $('.middleRight').text()] ,
+               [ $('.bottomLeft').text(), $('.bottomMiddle').text(), $('.bottomRight').text()] ,
+
+               [ $('.topLeft').text(), $('.middleLeft').text(), $('.bottomLeft').text()] ,
+               [ $('.topMiddle').text(), $('.middleMiddle').text(), $('.bottomMiddle').text()] ,
+               [ $('.topRight').text(), $('.middleRight').text(), $('.bottomRight').text()],
+
+               [ $('.topLeft').text(), $('.middleMiddle').text(), $('.bottomRight').text()],
+               [ $('.topRight').text(), $('.middleMiddle').text(), $('.topLeft').text()]
+             ];
+}
+
+function winner(){
+  if(turn === true){
+    $board.forEach(function(){
+      if($(this).text() === "X", "X", "X"){
+        console.log("x wins!");
+      }
+    });
+  }
+  else{
+    $board.forEach(function(){
+      if($(this).text() === "O", "O", "O"){
+        console.log("o wins!");
+      }
+    });
   }
 }
