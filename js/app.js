bb1474eeca34cbbe18ea2e224320d9678832e8a0
sophn11 <sophianakpil@gmail.com>
2016-07-03 15:21:02 -0700

    add var playerTurn to switch from x and o and change color

--- js/app.js
+++ js/app.js
@@ -1,5 +1,24 @@
 $(document).ready(function() {
-  $(".box").click(function(event) {
+
+ var playerTurn = 0; // 9 turns in all
+
+ $(".box").click(function testBox(event) {
+   if ($(this).hasClass("took")){
+     alert("No Moves Here!");
+   }
+else{
+  if ( playerTurn % 2 === 0 ){
+    $(this).val("x");
     $(this).text("x");
-    });
+    $(this).css( "background-color" , "green" );
+    }
+  else if (playerTurn % 2 === 1) {
+    $(this).val("o");
+    $(this).text("o");
+    $(this).css( "background-color" , "yellow" );
+    }
+  }
+    $(this).addClass("took");
+    playerTurn++;
+  });
 });
