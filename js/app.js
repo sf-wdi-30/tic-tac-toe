528b40ae91781cc3d118c2de579f7387aba0d12d
billhimmelsbach <whimmels@gmail.com>
2016-07-04 09:17:36 -0700

    adds 6 attempts at creating a function that creates a workable array from style tags and titles, checkWin() is cleanest. Attempts included in comments for reference

--- js/app.js
+++ js/app.js
@@ -1,24 +1,54 @@
 // wait for the DOM to finish loading
 
-var checkForClass = function(array, x) {
-  $(array).is(x);
-};
-
-
-// var checkClass = function(array) {
-//   return array.is('.x');
+//TEST
+// var checkClass = function(b) {
+//   console.log(b.is('.x'));
 // };
+//
+// var topArray=[];
+// var arrayCreator = function() {
+//   for (i=0; i<($('.top').length-1); i++) {
+//   topArray.push(checkClass(topArray[i]));
+// }
+// };
+
+
+
+//THIS WORKS
+//
 // var checkClass = function(array) {
 //   return ($('.top').is('.x'));
 // };
-
-var topArray = $('.top');
 //
-var test2 = function() {
-  for (i=0; i<($('.top').length-1); i++) {
-  console.log(checkClass(topArray[i]));
-}
-};
+// var topArray=[];
+// var arrayCreator = function() {
+//   for (i=0; i<($('.top').length-1); i++) {
+//   topArray.push(checkClass(topArray[i]));
+// }
+// };
+
+//THIS WORKS
+
+
+// var winCheck = function() {
+//   if ("x"===$('#top-left').attr('title')) {
+//       console.log('cool');
+//     }
+//     else {
+//       console.log('not cool');
+//     }
+// };
+// var checkForClass = function(array, x) {
+//   $(array).is(x);
+// };
+//
+// var checkClass = function(array) {
+//   return array.is('.x');
+// };
+
+// var topArray = $('.top');
+//
+
 // $.each($('.top'), function( key, value ) {
 
 
@@ -38,8 +68,65 @@ var test2 = function() {
   // }
 // $(stuff)each.hasClass( "foo" );
 // $(stuff).hasClass( "bar" )
-};
+// };
+//
+// var test = function() {
+//  $('.top').each($('.top').attr('title'));
+// };
+//
+// var winCheck = function() {
+//   $('.top').attr('title');
+// };
+// myArray=[];
+// // var test3 = $.each($('.top').myArray.push.attr('title'));
+// var top = $('.top');
 
+// function test4() {
+//     $('.top').each(function () {
+//       var testArray = [];
+//     testArray.fill($(this).attr('title'), 0);
+//     console.log(testArray);
+//     });
+//
+// }
+
+// function test4() {
+//     $('.top').each(function () {
+//         var testdata = $(this).attr('title');
+//         //$(this).val(testdata);
+//         console.log(testdata);
+//     });
+//
+// }
+
+// var test3 = function () {
+//   jQuery.each(($('.top').attr('title')), function(index, value) {
+//       console.log('element at index ' + $(index).attr('title') + ' is ' + value);
+//     });
+// } ;
+
+// var test3 = function () {
+//   jQuery.each(($('.top')), function(index, value) {
+//       console.log('element at index ' + $(index).attr('title') + ' is ' + value);
+//     });
+// } ;
+//
+// var test3 = function () {
+//   jQuery.each(($('.top')), function(index, value) {
+//       console.log('element at index ' + index + ' is ' + value);
+//     });
+// } ;
+
+// jQuery.each(($('.top')), function(index, value) {
+//     console.log('element at index ' + index + ' is ' + value);
+// });
+function checkWin() {
+    var testArray = [];
+    $('.top').each(function () {
+        testArray.push($(this).attr('title'));
+        console.log(testArray);
+    });
+}
 var xIcon = ('url("img/xIcon.png")');
 var oIcon = ('url("img/oIcon.png")');
 //set starting player
@@ -74,9 +161,11 @@ $(document).ready(function() {
   $(this).addClass("filled");
   if (playerIcon === (xIcon)) {
     $(this).addClass("x");
+    $(this).attr('title', "x");
   }
   else {
     $(this).addClass("o");
+    $(this).attr('title', "o");
   }
   playerSwitch(this);
 }
