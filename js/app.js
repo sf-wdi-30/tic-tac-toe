02f7924ea87d34c434419507979caaf5508ec2f9
mattericbrown <matt.eric.brown@gmail.com>
2016-07-04 15:57:50 -0700

    update to app.js that can now detect a winner and a cats game (only when the game is complete)

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  board = $('.box').toArray(' ');
+  board = $('.box').toArray('');
  $('.box').on('click', function(){
    $(this).css('background-image', whoseTurn );
    moves ++;
@@ -15,28 +15,62 @@ var moves = 0;
 var board;
 console.log (board);
 var whoseTurn = function(){
- for (var i = moves; i < 9; i){
+ for (var i = moves; i <= 8; i){
    if (moves % 2 === 0){
-     $(this).attr('boolean', true);
+     $(this).attr('boolean', 'true');
      return x;
    }else if (moves % 2 ===1){
-     $(this).attr('boolean', false);
+     $(this).attr('boolean', 'false');
      return o;
-   }
- }
+     }
+    }
+  checkWinner();
+  legalMove();
 };
-function legalMove(){
 
-
+var legalMove = function(){
+if($(this).attr('boolean') !== 'true' || 'false'){
+  return true;
 }
+console.log(legalMove);
 
-function whoWins(){
+};
 
-
-
-
+function checkWinner(){
+  if ($(board[0]).attr('boolean') === 'true' && $(board[1]).attr('boolean') === 'true' && $(board[2]).attr('boolean') === 'true'){
+    alert("Young Shia has won!");
+  }else if ($(board[0]).attr('boolean') === 'false' && $(board[1]).attr('boolean') === 'false' && $(board[2]).attr('boolean') === 'false'){
+  alert("Old Shia has won!");
+}else if ($(board[3]).attr('boolean') === 'true' && $(board[4]).attr('boolean') === 'true' && $(board[5]).attr('boolean') === 'true'){
+  alert("Young Shia has won!");
+}else if ($(board[3]).attr('boolean') === 'false' && $(board[4]).attr('boolean') === 'false' && $(board[5]).attr('boolean') === 'false'){
+  alert("Old Shia has won!");
+}else if ($(board[6]).attr('boolean') === 'true' && $(board[7]).attr('boolean') === 'true' && $(board[8]).attr('boolean') === 'true'){
+  alert("Young Shia has won!");
+}else if ($(board[6]).attr('boolean') === 'false' && $(board[7]).attr('boolean') === 'false' && $(board[8]).attr('boolean') === 'false'){
+alert("Old Shia has won!");
+}else if ($(board[0]).attr('boolean') === 'true' && $(board[3]).attr('boolean') === 'true' && $(board[6]).attr('boolean') === 'true'){
+alert("Young Shia has won!");
+}else if ($(board[0]).attr('boolean') === 'false' && $(board[3]).attr('boolean') === 'false' && $(board[6]).attr('boolean') === 'false'){
+alert("Old Shia has won!");
+}else if ($(board[1]).attr('boolean') === 'true' && $(board[4]).attr('boolean') === 'true' && $(board[7]).attr('boolean') === 'true'){
+alert("Young Shia has won!");
+}else if ($(board[1]).attr('boolean') === 'false' && $(board[4]).attr('boolean') === 'false' && $(board[7]).attr('boolean') === 'false'){
+alert("Old Shia has won!");
+}else if ($(board[2]).attr('boolean') === 'true' && $(board[5]).attr('boolean') === 'true' && $(board[8]).attr('boolean') === 'true'){
+alert("Young Shia has won!");
+}else if ($(board[2]).attr('boolean') === 'false' && $(board[5]).attr('boolean') === 'false' && $(board[8]).attr('boolean') === 'false'){
+alert("Old Shia has won!");
+}else if ($(board[0]).attr('boolean') === 'true' && $(board[4]).attr('boolean') === 'true' && $(board[8]).attr('boolean') === 'true'){
+alert("Young Shia has won!");
+}else if ($(board[0]).attr('boolean') === 'false' && $(board[4]).attr('boolean') === 'false' && $(board[8]).attr('boolean') === 'false'){
+alert("Old Shia has won!");
+}else if ($(board[2]).attr('boolean') === 'true' && $(board[4]).attr('boolean') === 'true' && $(board[6]).attr('boolean') === 'true'){
+alert("Young Shia has won!");
+}else if ($(board[2]).attr('boolean') === 'false' && $(board[4]).attr('boolean') === 'false' && $(board[6]).attr('boolean') === 'false'){
+alert("Old Shia has won!");
+}else {
+  alert("Just like watching the Transformers films, nobody wins");
 }
-
-
-
+}
 console.log(board);
