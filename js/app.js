cd22d5011e7804a766761fb1bd1bab303ff63a45
mattericbrown <matt.eric.brown@gmail.com>
2016-07-03 18:12:18 -0700

    added variables, and on click function and created an empty array in apps.js

--- js/app.js
+++ js/app.js
@@ -2,5 +2,37 @@
 $(document).ready(function() {
   // all code to manipulate the DOM
   // goes inside this function
+  $('.box').on('click', function(){
+    $(this).css('background-image', whoseTurn );
+  });
   console.log("check");
 });
+
+//variables to input into the gameboard array
+var x = 'url(http://i.imgur.com/445g4BV.png)';
+var o = 'url(http://i.imgur.com/5WVUarW.png)';
+var start = ' ';
+
+//variables for the start of the game
+var firstPlayer = x;
+var moves = 0;
+var board = $('.box').toArray();
+console.log (board);
+
+
+
+/*
+whoseTurn = function(){
+  for(var i = moves; i<9; i++);
+  if (i % 2 === 0){
+    return true;
+  } else {
+    return false;
+  }
+  if (true){
+    board.unshift(0);
+  } else {
+    board.unshift(0);
+  }
+};
+*/
