b0e406c7f97bd947232707865f76804bbbb9abc4
jamestothe <jamesdelacruz@gmail.com>
2016-07-04 09:43:24 -0700

    added some missing winning combinations to js  arrays]

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 // wait for the DOM to finish loading
 alert("Sanity Check!");
-var boxesRow1 = [".box0", ".box3", ".box6"];
-var boxesRow2 = [".box1", ".box4", ".box7"];
-var boxesRow3 = [".box2", ".box5", ".box28"];
+var boxesRow1 = [".box0", ".box3", ".box6", ".box2", ".box0", ".box0", ".box1", ".box2"];
+var boxesRow2 = [".box1", ".box4", ".box7", ".box4", ".box4", ".box3", ".box4", ".box5"];
+var boxesRow3 = [".box2", ".box5", ".box8", ".box6", ".box8", ".box6", ".box7", ".box8"];
 
 $(document).ready(function() {
 //   // all code to manipulate the DOM
@@ -14,6 +14,9 @@ $(document).ready(function() {
       if ($(boxesRow1[i]).val() === "O" && $(boxesRow2[i]).val() === "O" && $(boxesRow3[i]).val() === "O"){
         alert("O wins!");
       }
+      else if ($(boxesRow1[i]).val() === "X" && $(boxesRow2[i]).val() === "X" && $(boxesRow3[i]).val() === "X"){
+        alert("X wins!");
+      }
   }
 
   $(".box").on("click", function checkBox(event){ // Main enginge logic starts here event listener user delegation? ASK!
@@ -21,24 +24,26 @@ $(document).ready(function() {
         alert("Can't move here");
       }
       else{ // else will execute if an open square is selected (houses the rest of the program)
-          if (turnCounter % 2 === 0){ // nested conditional statement that adds color and value to squares
+          if(turnCounter === 9){
+            alert("DRAW");
+          }
+          else if (turnCounter % 2 === 0){ // nested conditional statement that adds color and value to squares
             $(this).val("X");
             $(this).text("X");
             $(this).css("color", ("blue"));
-            winChecker();
-            alert("O's turn");
             //$(this).css('background-color', ("blue"));
           }
           else if (turnCounter % 2 === 1){
             $(this).val("O");
             $(this).text("O");
             $(this).css("color", ("red"));
-            winChecker();
-            alert("X's turn");
             //$(this).css('background-color', ("red"));
           }
+
+      winChecker();
+      alert($(this).val() + " turn");
       $(this).addClass("took");
-      turnCounter++;
+
         // if((turnCounter%2) === 1){  // conditional statement that alerts which player's turn it is
         //   alert("O's turn");
         // }
@@ -47,6 +52,7 @@ $(document).ready(function() {
         // }
 
       }
+        turnCounter++;
     });
     $('.reset').on("click", function handleClick(e){ // reset button works by removing classes
       $('.box').removeClass("took");
