ba8f0ba8953d5748b9bc95221573adbc407a17af
WDIDavidKim <davidjkim1984@gmail.com>
2016-07-04 01:19:20 -0700

    updated js to change background on click

--- js/app.js
+++ js/app.js
@@ -1,39 +1,35 @@
 // wait for the DOM to finish loading
 $(document).ready(function() {
+
   // all code to manipulate the DOM
   // goes inside this functionz
-var boxesOnBoard = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  if ([0,1,2]){
-    alert("You have won!");
-  }else if ([3,4,5]){
-    alert("You have won!");
-  }else if ([6,7,8]);
-    alert("You have won!");
-  }else if ([0,3,6]);
-    alert("You have won!");
-  }else if ([1,4,7]);
-    alert("You have won!");
-  }else if ([2,5,8]);
-    alert("You have won!");
-  }else if ([0,4,8]);
-    alert("You have won!");
-  }else if ([2,4,6]);
-    alert("You have won!");
 
-
-  //}else ;{
-    //alert("This game is a tie.")
-
-
-
+$(".box").on("click", function clicked(){
+  var boxes = $(this);
+  boxes.css("background" , "url(http://i.imgur.com/ZYIvSdV.jpg?3");
+  });
 });
 
-
-
-
-
-
-
+//rules
+//var matchesOnBoard = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  //if ([0,1,2]){
+    //alert("You have won!");
+  //}else if ([3,4,5]){
+    //alert("You have won!");
+  //}else if ([6,7,8]);
+    //alert("You have won!");
+  //}else if ([0,3,6]);
+    //alert("You have won!");
+  //}else if ([1,4,7]);
+    //alert("You have won!");
+  //}else if ([2,5,8]);
+    //alert("You have won!");
+  //}else if ([0,4,8]);
+    //alert("You have won!");
+  //}else if ([2,4,6]);
+    //alert("You have won!");
+  //}else ;{
+    //alert("This game is a tie.")
 
 
 
