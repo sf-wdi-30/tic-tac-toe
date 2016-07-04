cca8fb87f5f964c9e9ec26d2c2c0b76ac768d5e9
tbduong <tiffaniduong@gmail.com>
2016-07-04 12:51:05 -0700

    add condition to determine if box has element inside already

--- js/app.js
+++ js/app.js
@@ -1,11 +1,21 @@
 // wait for the DOM to finish loading
 $(document).ready(function() {
 
+//Which player is currently going?
+
+var player = 1;
+
+//clicked on square
     $(".col-md-4").on("click" , function(event) {
 
       alert("Draw here!");
-
       var boxClicked = $(this);
+//determine if there is element in box
+      if(boxClicked.hasClass('ex') || boxClicked.hasClass('oh')) {
+          alert('This square has already been clicked.');}
+          
+      }
+
     });
 
 });
