db53532e98e57aa984be75f7215690b54fb7566f
mehgellan <baumegg@gmail.com>
2016-07-03 19:15:14 -0700

    create logic for switching turns

--- js/app.js
+++ js/app.js
@@ -8,5 +8,21 @@ $(document).ready(function() {
 
 // possible way to set up board
 var boxes = $(".box");
-var firstBox = boxes.eq(0);
-var secondBox = boxes.eq(1);
+var box1 = boxes.eq(0);
+var box2 = boxes.eq(1);
+var box3 = boxes.eq(2);
+var box4 = boxes.eq(3);
+var box5 = boxes.eq(4);
+var box6 = boxes.eq(5);
+var box7 = boxes.eq(6);
+var box8 = boxes.eq(7);
+var box9 = boxes.eq(8);
+
+var turn = '';
+function nextTurn() {
+  if (turn==='x') {
+    turn = 'o';
+  } else {
+    turn = 'x';
+  }
+}
