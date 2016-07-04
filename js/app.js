24619fda5cbbb3e14ed391b1fe313044d3543044
mattericbrown <matt.eric.brown@gmail.com>
2016-07-04 13:52:59 -0700

    added styling to the 'reset' form in css, updated game logic in app.js, changed formatting of index.html

--- js/app.js
+++ js/app.js
@@ -1,38 +1,42 @@
-// wait for the DOM to finish loading
 $(document).ready(function() {
-  // all code to manipulate the DOM
-  // goes inside this function
-  $('.box').on('click', function(){
-    $(this).css('background-image', whoseTurn );
-  });
-  console.log("check");
+  board = $('.box').toArray(' ');
+ $('.box').on('click', function(){
+   $(this).css('background-image', whoseTurn );
+   moves ++;
+ });
+ console.log("check");
 });
-
 //variables to input into the gameboard array
 var x = 'url(http://i.imgur.com/445g4BV.png)';
 var o = 'url(http://i.imgur.com/5WVUarW.png)';
 var start = ' ';
-
 //variables for the start of the game
-var firstPlayer = x;
 var moves = 0;
-var board = $('.box').toArray();
+var board;
 console.log (board);
-
-
-
-/*
-whoseTurn = function(){
-  for(var i = moves; i<9; i++);
-  if (i % 2 === 0){
-    return true;
-  } else {
-    return false;
-  }
-  if (true){
-    board.unshift(0);
-  } else {
-    board.unshift(0);
-  }
+var whoseTurn = function(){
+ for (var i = moves; i < 9; i){
+   if (moves % 2 === 0){
+     $(this).attr('boolean', true);
+     return x;
+   }else if (moves % 2 ===1){
+     $(this).attr('boolean', false);
+     return o;
+   }
+ }
 };
-*/
+function legalMove(){
+
+
+}
+
+function whoWins(){
+
+
+
+
+}
+
+
+
+console.log(board);
