ec589a971b657da3f8c674ec15ebd68bf28910cd
tbduong <tiffaniduong@gmail.com>
2016-07-04 13:44:44 -0700

    create if conditions for all horizontal win possibilities

--- js/app.js
+++ js/app.js
@@ -9,21 +9,34 @@ var player = 1;
     var boxClicked = $(this);
 
 //alternate turns and determine if box is taken
+//Symbols: 'ex' and oh'
       if(boxClicked.hasClass('ex') || boxClicked.hasClass('oh')) {
           alert("This box is taken!");
       } else {
           if(player === 1) {
-            boxClicked.addClass('ex');
+            boxClicked.addClass("ex");
             player = 2;
          } else {
-            boxClicked.addClass('oh');
+            boxClicked.addClass("oh");
             player = 1;
            }
 
       }
     });
-//Check if someone won?--- create function that checks symbol X or O? ('ex' and 'oh')
-  function checkWin(){
+//Check if someone won?--- create function that checks symbol X or O? ('ex' and 'oh')...
+//list ALL winning possibilities? (check to see if works)
+//horizontal rows
+  function checkWin(symbol) {
+    if($("#1").hasClass(symbol) && $("#2").hasClass(symbol) && $("#3").hasClass(symbol)) {
+      return true;
+
+    } else if ($("#4").hasClass(symbol) && $("#5").hasClass(symbol) && $("#6").hasClass(symbol)) {
+      return true;
+
+    } else if ($("#7").hasClass(symbol) && $("#8").hasClass(symbol) && $("#9").hasClass(symbol)) {
+    } else {
+        return false;
+    }
 
   }
 
