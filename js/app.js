8e0581fda743c677836b93035e9b8fb93bdf531c
mehgellan <baumegg@gmail.com>
2016-07-03 18:38:05 -0700

    set up possible board layout with jQuery

--- js/app.js
+++ js/app.js
@@ -2,5 +2,11 @@
 $(document).ready(function() {
   // all code to manipulate the DOM
   // goes inside this function
-
+  alert("JS is linked to html!");
 });
+
+
+// possible way to set up board
+var boxes = $(".box");
+var firstBox = boxes.eq(0);
+var secondBox = boxes.eq(1);
