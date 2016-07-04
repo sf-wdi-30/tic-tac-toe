c26539e429a1e2ec4349c3c55f6ef35a4efc996e
WDIDavidKim <davidjkim1984@gmail.com>
2016-07-04 03:17:39 -0700

    notes for adding another player to to possibly block the other. changed font style css

--- js/app.js
+++ js/app.js
@@ -1,20 +1,27 @@
 // wait for the DOM to finish loading
 $(document).ready(function() {
-var player1 ="X";
+var player1 = "X";
+//var player2 = "O"; //use this while trying to block the other box
   // all code to manipulate the DOM
   // goes inside this functionz
 
-$(".box").on("click", function clicked(){
-  var boxes = $(this);
+$(".box").on("click", function clicked(event){
+  var markedBoxes = $(this);
+
   if(player1===0){
     player1 = 1;
-      boxes.css("background" , "url(http://i.imgur.com/OhWUAEc.jpg?4");
+      markedBoxes.css("background" , "url(http://i.imgur.com/OhWUAEc.jpg?4");//O
   }else{
     player1 = 0;
-      boxes.css("background" , "url(http://i.imgur.com/KVrSf2j.png?6");
+      markedBoxes.css("background" , "url(http://i.imgur.com/KVrSf2j.png?6");//X
     }
   });
 });
+
+$(".btn").on("click", function clear(event){
+
+})
+
 
 //rules
 //var matchesOnBoard = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
