f79f9c2e57e55e4af5f45d9cb69ba036b073a9c7
sophn11 <sophianakpil@gmail.com>
2016-07-05 09:06:53 -0700

    added images for x and o

--- js/app.js
+++ js/app.js
@@ -10,12 +10,12 @@ else{
   if ( playerTurn % 2 === 0 ){
     $(this).val("x");
     $(this).text("x");
-    $(this).css( "background-color" , "green" );
+    $(this).css( "background-image" , 'url("img/tattoofail.jpg")' );
     }
   else if (playerTurn % 2 === 1) {
     $(this).val("o");
     $(this).text("o");
-    $(this).css( "background-color" , "yellow" );
+    $(this).css( "background-image" , 'url("img/birdstransparent.jpg")' );
     }
   }
     $(this).addClass("took");
@@ -29,7 +29,7 @@ else{
 $(".btn").on('click', function(event) {
   $(".box").each (function (event) {
      $(this).html(' ');
-     $(this).css('background-color' , 'white');
+     $(this).css('background-image' , 'none');
      $(this).removeClass("took");
    });
  });
