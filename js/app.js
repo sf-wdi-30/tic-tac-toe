1ad57a2aa9b6762845c9720788921a53fe9f8134
billhimmelsbach <whimmels@gmail.com>
2016-07-05 00:07:58 -0700

    clean js file, correct tabs, add explanatory comments

--- js/app.js
+++ js/app.js
@@ -1,190 +1,16 @@
-// wait for the DOM to finish loading
-
-//TEST
-// var checkClass = function(b) {
-//   console.log(b.is('.x'));
-// };
-//
-// var topArray=[];
-// var arrayCreator = function() {
-//   for (i=0; i<($('.top').length-1); i++) {
-//   topArray.push(checkClass(topArray[i]));
-// }
-// };
-
-
-
-//THIS WORKS
-//
-// var checkClass = function(array) {
-//   return ($('.top').is('.x'));
-// };
-//
-// var topArray=[];
-// var arrayCreator = function() {
-//   for (i=0; i<($('.top').length-1); i++) {
-//   topArray.push(checkClass(topArray[i]));
-// }
-// };
-
-//THIS WORKS
-
-
-// var winCheck = function() {
-//   if ("x"===$('#top-left').attr('title')) {
-//       console.log('cool');
-//     }
-//     else {
-//       console.log('not cool');
-//     }
-// };
-// var checkForClass = function(array, x) {
-//   $(array).is(x);
-// };
-//
-// var checkClass = function(array) {
-//   return array.is('.x');
-// };
-
-// var topArray = $('.top');
-//
-
-// $.each($('.top'), function( key, value ) {
-
-
-// var test2 = function() {
-// $.each($('.top'), function( key, value ) {
-//   console.log(checkClass());
-// });
-// };
-
-// var test = function() {
-//   jQuery.each($('.top'), checkClass() );
-// };
-//
-// var checkWin = function(top) {
-//   $.each(top, checkForClass());
-//   //   .hasClass( "bar" )
-  // }
-// $(stuff)each.hasClass( "foo" );
-// $(stuff).hasClass( "bar" )
-// };
-//
-// var test = function() {
-//  $('.top').each($('.top').attr('title'));
-// };
-//
-// var winCheck = function() {
-//   $('.top').attr('title');
-// };
-// myArray=[];
-// // var test3 = $.each($('.top').myArray.push.attr('title'));
-// var top = $('.top');
-
-// function test4() {
-//     $('.top').each(function () {
-//       var testArray = [];
-//     testArray.fill($(this).attr('title'), 0);
-//     console.log(testArray);
-//     });
-//
-// }
-
-// function test4() {
-//     $('.top').each(function () {
-//         var testdata = $(this).attr('title');
-//         //$(this).val(testdata);
-//         console.log(testdata);
-//     });
-//
-// }
-
-// var test3 = function () {
-//   jQuery.each(($('.top').attr('title')), function(index, value) {
-//       console.log('element at index ' + $(index).attr('title') + ' is ' + value);
-//     });
-// } ;
-
-// var test3 = function () {
-//   jQuery.each(($('.top')), function(index, value) {
-//       console.log('element at index ' + $(index).attr('title') + ' is ' + value);
-//     });
-// } ;
-//
-// var test3 = function () {
-//   jQuery.each(($('.top')), function(index, value) {
-//       console.log('element at index ' + index + ' is ' + value);
-//     });
-// } ;
-
-// jQuery.each(($('.top')), function(index, value) {
-//     console.log('element at index ' + index + ' is ' + value);
-// });
-
-function checkWin(condition) {
-    var testArray = [];
-    condition.each(function () {
-        testArray.push($(this).attr('title'));
-      });
-        var testString = testArray.toString();
-        var overlay = jQuery('<div class="overlay"><h1 class="winnerMessage"></h1><br class="beforeImg"></div>');
-        if (testString === "x,x,x") {
-        $('#board div div').addClass("filled");
-        // alert("X Wins");
-        turnCounter=0;
-        gameWon=1;
-        $('.alertDisplay').text(" ");
-        overlay.appendTo(document.body);
-        $('.winnerMessage').html("JAY-Z AND PLAYER X WIN! <br><br> TAKE YOUR ASS TO RED LOBSTER!");
-        $( ".beforeImg" ).after("<img class='overlayGif' src='img/jayBow.gif'>");
-        }
-        else if (testString === "o,o,o") {
-        $('#board div div').addClass("filled");
-        // alert("O Wins");
-        turnCounter=0;
-        gameWon=1;
-        $('.alertDisplay').text(" ");
-        overlay.appendTo(document.body);
-        $('.winnerMessage').html("WHO RUNS THE WORLD? <br><br> BEYONCÉ AND PLAYER O!");
-        $( ".beforeImg" ).after("<img class='overlayGif' src='img/beyonceDance.gif'>");
-        }
-        else {
-        console.log("NOPE");
-        }
-}
-//**
-// FOR LATER: RETURN A FUNCTION INSTEAD OF ALERT win(x) or win(y)
-//**
-
-function displayTurn() {
-  if (playerIcon === oIcon) {
-    $('.alertDisplay').text("Player O's Turn");
-  }
-  else {
-    $('.alertDisplay').text("Player X's Turn");
-  }
-}
-
-
-// function checkFull {
-//   $('#board div div')
-//   if ($(this).hasClass('filled'))
-// }
-
-
-// failed experiment with for loop for iterating on winCondition();
-// winConditions = ["$(.'top')",
-// // ".'middle'", ".'bottom'", "'left'", "'center'", "'right'", "'diag1'", "'diag2'"
-// ];
+//declartion of variables
 
 var xIcon = ('url("img/xIcon.png")');
 var oIcon = ('url("img/oIcon.png")');
+
+//event state toggles
 var turnCounter = 0;
 var gameWon = 0;
 var gifMode = 0;
 
 //set starting player
 var playerIcon = oIcon;
+
 //switch player function
 var playerSwitch = function() {
   if (playerIcon === xIcon) {
@@ -195,8 +21,54 @@ var playerSwitch = function() {
   }
 };
 
+//function that checks a jQuery argument for a win condition
+function checkWin(condition) {
+    var testArray = [];
+    condition.each(function () {
+      //push attributes into an array
+      testArray.push($(this).attr('title'));
+    });
+    var testString = testArray.toString();
+    //setup variable with contents of overlay
+    var overlay = jQuery('<div class="overlay"><h1 class="winnerMessage"></h1><br class="beforeImg"></div>');
+    if (testString === "x,x,x") {
+      $('#board div div').addClass("filled");
+      turnCounter=0;
+      gameWon=1;
+      $('.alertDisplay').text(" ");
+      overlay.appendTo(document.body);
+      $('.winnerMessage').html("JAY-Z AND PLAYER X WIN! <br><br> TAKE YOUR ASS TO RED LOBSTER!");
+      $( ".beforeImg" ).after("<img class='overlayGif' src='img/jayBow.gif'>");
+    }
+    else if (testString === "o,o,o") {
+      $('#board div div').addClass("filled");
+      turnCounter=0;
+      gameWon=1;
+      $('.alertDisplay').text(" ");
+      overlay.appendTo(document.body);
+      $('.winnerMessage').html("WHO RUNS THE WORLD? <br><br> BEYONCÉ AND PLAYER O!");
+      $( ".beforeImg" ).after("<img class='overlayGif' src='img/beyonceDance.gif'>");
+    }
+      else {
+        //sanity check
+        console.log("NOPE");
+    }
+  }
+
+//function that switches players' turns
+function displayTurn() {
+  if (playerIcon === oIcon) {
+    $('.alertDisplay').text("Player O's Turn");
+  }
+  else {
+    $('.alertDisplay').text("Player X's Turn");
+  }
+}
+
+//on document load main function
 $(document).ready(function() {
-displayTurn();
+  //display turn to fill alertDisplay
+  displayTurn();
   //reset button
   $('.reset').on('click', function(event) {
     $(".overlay").remove();
@@ -210,78 +82,76 @@ displayTurn();
 //I Love GIFs button
 $('.gifButton').on('click', function(event) {
   if (gifMode===0) {
-    console.log(gifMode);
-  $("div").css('background-image', "");
-  $("div").removeClass('filled');
-  $("div").attr('title', " ");
-  gameWon=0;
-  turnCounter=0;
-  displayTurn();
-  $('.box').css('background-size', '100%');
-  oIcon = ('url("img/beyonce.gif")');
-  xIcon = ('url("img/jay.gif")');
-  gifMode=1;
-  playerIcon = oIcon;
-  $(this).html("I HATE GIFS");
+    $("div").css('background-image', "");
+    $("div").removeClass('filled');
+    $("div").attr('title', " ");
+    gameWon=0;
+    turnCounter=0;
+    displayTurn();
+    $('.box').css('background-size', '100%');
+    oIcon = ('url("img/beyonce.gif")');
+    xIcon = ('url("img/jay.gif")');
+    gifMode=1;
+    playerIcon = oIcon;
+    $(this).html("I HATE GIFS");
   }
-    else {
-    console.log(gifMode);
-  $("div").css('background-image', "");
-  $("div").removeClass('filled');
-  $("div").attr('title', " ");
-  gameWon=0;
-  turnCounter=0;
-  displayTurn();
-  $('.box').css('background-size', '60%');
-  oIcon = ('url("img/oIcon.png")');
-  xIcon = ('url("img/xIcon.png")');
-  playerIcon = oIcon;
-  gifMode=0;
-  $(this).html("I &#9829; GIFs");
+  else {
+    $("div").css('background-image', "");
+    $("div").removeClass('filled');
+    $("div").attr('title', " ");
+    gameWon=0;
+    turnCounter=0;
+    displayTurn();
+    $('.box').css('background-size', '60%');
+    oIcon = ('url("img/oIcon.png")');
+    xIcon = ('url("img/xIcon.png")');
+    playerIcon = oIcon;
+    gifMode=0;
+    $(this).html("I &#9829; GIFs");
   }
 });
 
-//draw event
+//draw X or O event
   $('#board').on('click', "div div", function(event) {
+    //cat's game prevent further actions
     if ((($(this).hasClass('filled'))) || (turnCounter===10)) {
     }
     else {
-  turnCounter += 1;
-  $(this).css('background-image', playerIcon);
-  $(this).addClass("filled");
-  if (playerIcon === (xIcon)) {
-    $(this).addClass("x");
-    $(this).attr('title', "x");
-  }
-  else {
-    $(this).addClass("o");
-    $(this).attr('title', "o");
-  }
-  playerSwitch(this);
-  //brute force solution; I tried a dozen different methods to put this in a for loop, but jQuery refused to accept the variable
-  checkWin($('.top'));
-  checkWin($('.middle'));
-  checkWin($('.bottom'));
-  checkWin($('.left'));
-  checkWin($('.center'));
-  checkWin($('.right'));
-  checkWin($('.diag1'));
-  checkWin($('.diag2'));
-  console.log(turnCounter);
-  if (gameWon===1){
-    console.log("test");
-  }
-  else {
-    displayTurn();
-  if (turnCounter===9) {
-    $('.alertDisplay').text("It's a draw!");
-  }
-}
-  console.log("done");
-}
-}
-);
+      turnCounter += 1;
+      //changes background image
+      $(this).css('background-image', playerIcon);
+      //mark as filled, preventing further clicks
+      $(this).addClass("filled");
+      //adds x or o to class and title
+      if (playerIcon === (xIcon)) {
+        $(this).addClass("x");
+        $(this).attr('title', "x");
+      }
+      else {
+        $(this).addClass("o");
+        $(this).attr('title', "o");
+      }
+      //switch the player for next round
+      playerSwitch(this);
+      //brute force solution; I tried a dozen different methods to put this in a for loop, but jQuery refused to accept the variable
+      checkWin($('.top'));
+      checkWin($('.middle'));
+      checkWin($('.bottom'));
+      checkWin($('.left'));
+      checkWin($('.center'));
+      checkWin($('.right'));
+      checkWin($('.diag1'));
+      checkWin($('.diag2'));
+      //cat's game check
+      if (gameWon===1){
+      }
+      else {
+        displayTurn();
+      if (turnCounter===9) {
+        $('.alertDisplay').text("It's a draw!");
+      }
+      }
+      }
+    }
+  );
 });
-
-// $(this).css('background-color', 'blue');
-//   %(this).attr()
