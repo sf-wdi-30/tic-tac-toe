4c8059afc8449cdce875ee7e6e65ce269e057f5a
WDIDavidKim <davidjkim1984@gmail.com>
2016-07-04 22:40:33 -0700

    Logic did not work

--- js/app.js
+++ js/app.js
@@ -9,56 +9,47 @@ var player = 1;
     var markedBoxes = $(this);
 
     if(markedBoxes.hasClass('X') || markedBoxes.hasClass('O')){
-      alert('This box has already been selected!');//Still switches images even after alert.
-      markedBoxes('X') !== markedBoxes('O');//this stops from switching colors.
+      alert('This box has already been selected!'); //Still switches images even after alert.
+      markedBoxes('X') !== markedBoxes('O'); //this stops from switching colors.
+    }else{
+      if(player === 1){
+        markedBoxes.addClass('X');
+        player = 2;
       }else{
-        if(player === 1){
-          markedBoxes.addClass('X');
-          if(winningCombo('X')){
-            alert('You have won!');
-          }else{
-          player = 2;
-          }
-          }else{
-          markedBoxes.addClass('O');
-          if(winningCombo('O')){
-            alert('You have won!');
-          }else{
-          player = 1;
-        }
+        markedBoxes.addClass('O');
+
+      player = 1;//So that it alternates image and player.  If not will stay on the second players img and turn.
       }
-
-    //}else{
-      //markedBoxes.addClass('O');
-
-      //player = 1;//So that it alternates image and player.  If not will stay on the second players img and turn.
     }
   });
 });
-function winningCombo(winner){
-  if($('.1').hasClass(winner) && $('.2').hasClass(winner) && $('.3').hasClass(winner)){
-    return true;
-    }else if($('.4').hasClass(winner) && $('.5').hasClass(winner) && $('.6').hasClass(winner)){
-      return true;
-    }else if($('.7').hasClass(winner) && $('.8').hasClass(winner) && $('.9').hasClass(winner)){
-      return true;
-    }else if($('.1').hasClass(winner) && $('.4').hasClass(winner) && $('.7').hasClass(winner)){
-      return true;
-    }else if($('.2').hasClass(winner) && $('.5').hasClass(winner) && $('.8').hasClass(winner)){
-      return true;
-    }else if($('.3').hasClass(winner) && $('.6').hasClass(player) && $('.9').hasClass(winner)){
-      return true;
-    }else if($('.1').hasClass(winner) && $('.5').hasClass(player) && $('.9').hasClass(winner)){
-        return true;
-    }else if($('.3').hasClass(winner) && $('.5').hasClass(player) && $('.7').hasClass(winner)){
-          return true;
-    }else{
-      return false;
-  }
-}
 
-//$('.btn').on("click", function clear(event){
-  //$('.row').reset("");//cant get this to work.
+$('.reset').click(function() {
+  reset(table);
+});
+//function winningCombo(winner){
+  //if($('.1').hasClass(winner) && $('.2').hasClass(winner) && $('.3').hasClass(winner)){
+    //return true;
+    //}else if($('.1').hasClass(winner) && $('.2').hasClass(winner) && $('.3').hasClass(winner)){
+      //return true;
+    //}else if($('.4').hasClass(winner) && $('.5').hasClass(winner) && $('.6').hasClass(winner)){
+      //return true;
+    //}else if($('.7').hasClass(winner) && $('.8').hasClass(winner) && $('.9').hasClass(winner)){
+      //return true;
+    //}else if($('.1').hasClass(winner) && $('.4').hasClass(winner) && $('.7').hasClass(winner)){
+      //return true;
+    //}else if($('.2').hasClass(winner) && $('.5').hasClass(winner) && $('.8').hasClass(winner)){
+      //return true;
+    //}else if($('.3').hasClass(winner) && $('.6').hasClass(player) && $('.9').hasClass(winner)){
+      //return true;
+    //}else if($('.1').hasClass(winner) && $('.5').hasClass(player) && $('.9').hasClass(winner)){
+      //return true;
+    //}else if($('.3').hasClass(winner) && $('.5').hasClass(player) && $('.7').hasClass(winner)){
+      //return true;
+    //}else{
+      //return false;
+    //}
+
 
 
 
