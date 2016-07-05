259a306ed3b13bdf56a52a26e47c3d0911155ee5
cabrown91 <cleo.aliciabrown@gmail.com>
2016-07-05 07:35:04 -0700

    add function to check winner scenarios

--- js/app.js
+++ js/app.js
@@ -53,5 +53,41 @@ $(document).ready(function() {
     };
 
 
+        checkWin = function () {
+            if ((a1 == a2 && a1 == a3 && (a1 == "x")) || //first row
+            (b1 == b2 && b1 == b3 && (b1 == "x")) || //second row
+            (c1 == c2 && c1 == c3 && (c1 == "x")) || //third row
+            (a1 == b1 && a1 == c1 && (a1 == "x")) || //first column
+            (a2 == b2 && a2 == c2 && (a2 == "x")) || //second column
+            (a3 == b3 && a3 == c3 && (a3 == "x")) || //third column
+            (a1 == b2 && a1 == c3 && (a1 == "x")) || //diagonal 1
+            (a3 == b2 && a3 == c1 && (a3 == "x")) //diagonal 2
+            ) {
+                xWins = true;
+                winAlert();
+
+            } else { // checks if O won
+                if ((a1 == a2 && a1 == a3 && (a1 == "o")) || //first row
+                (b1 == b2 && b1 == b3 && (b1 == "o")) || //second row
+                (c1 == c2 && c1 == c3 && (c1 == "o")) || //third row
+                (a1 == b1 && a1 == c1 && (a1 == "o")) || //first column
+                (a2 == b2 && a2 == c2 && (a2 == "o")) || //second column
+                (a3 == b3 && a3 == c3 && (a3 == "o")) || //third column
+                (a1 == b2 && a1 == c3 && (a1 == "o")) || //diagonal 1
+                (a3 == b2 && a3 == c1 && (a3 == "o")) //diagonal 2
+                ) {
+                    oWins = true;
+                    winAlert();
+
+                } else { // checks if all boxes filled + tie game
+                    if (((a1 == "x") || (a1 == "o")) && ((b1 == "x") || (b1 == "o")) && ((c1 == "x") || (c1 == "o")) && ((a2 == "x") || (a2 == "o")) && ((b2 == "x") || (b2 == "o")) && ((c2 == "x") || (c2 == "o")) && ((a3 == "x") || (a3 == "o")) && ((b3 == "x") || (b3 == "o")) && ((c3 == "x") || (c3 == "o"))) {
+                        alert("It's a tie!");
+                    }
+                }
+            }
+        };
+
+
+
 
 });
