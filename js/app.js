997698be614b5f17be52e497204d3a9b894ae34a
sophn11 <sophianakpil@gmail.com>
2016-07-05 21:19:52 -0700

    removed a's and tried to add google font

--- js/app.js
+++ js/app.js
@@ -10,18 +10,73 @@ else{
   if ( playerTurn % 2 === 0 ){
     $(this).val("x");
     $(this).text("x");
-    $(this).css( "background-image" , 'url("img/tattoofail.jpg")' );
+    $(this).css( "background-image" , 'url("img/shark.jpg")' );
+    checkWin();
     }
   else if (playerTurn % 2 === 1) {
     $(this).val("o");
     $(this).text("o");
-    $(this).css( "background-image" , 'url("img/birdstransparent.jpg")' );
+    $(this).css( "background-image" , 'url("img/seal.jpg")' );
+    checkWin();
     }
   }
     $(this).addClass("took");
     playerTurn++;
   });
 
+  function checkWin() {
+        if ($(".1st").val() === "o" && $(".2nd").val() === "o" && $(".3rd").val() === "o"){
+          alert("Seal Wins!");
+        }
+        else if ($(".4th").val() === "o" && $(".5th").val() === "o" && $(".6th").val() === "o"){
+          alert("Seal Wins!");
+        }
+        else if ($(".7th").val() === "o" && $(".8th").val() === "o" && $(".9th").val() === "o"){
+          alert("Seal Wins!");
+        }
+        else if ($(".1st").val() === "o" && $(".4th").val() === "o" && $(".7th").val() === "o"){
+          alert("Seal Wins!");
+        }
+        else if ($(".2nd").val() === "o" && $(".5th").val() === "o" && $(".8th").val() === "o"){
+          alert("Seal Wins!");
+        }
+        else if ($(".3rd").val() === "o" && $(".6th").val() === "o" && $(".9th").val() === "o"){
+          alert("Seal Wins!");
+        }
+        else if ($(".1st").val() === "o" && $(".5th").val() === "o" && $(".9th").val() === "o"){
+          alert("Seal Wins!");
+        }
+        else if ($(".3rd").val() === "o" && $(".5th").val() === "o" && $(".7th").val() === "o"){
+          alert("Seal Wins!");
+        }
+        else if ($(".1st").val() === "x" && $(".2nd").val() === "x" && $(".3rd").val() === "x"){
+          alert("Shark Wins!");
+        }
+        else if ($(".4th").val() === "x" && $(".5th").val() === "x" && $(".6th").val() === "x"){
+          alert("Shark Wins!");
+        }
+        else if ($(".7th").val() === "x" && $(".8th").val() === "x" && $(".9th").val() === "x"){
+          alert("Shark Wins!");
+        }
+        else if ($(".1st").val() === "x" && $(".4th").val() === "x" && $(".7th").val() === "x"){
+          alert("Shark Wins!");
+        }
+        else if ($(".2nd").val() === "x" && $(".5th").val() === "x" && $(".8th").val() === "x"){
+          alert("Shark Wins!");
+        }
+        else if ($(".3rd").val() === "x" && $(".6th").val() === "x" && $(".9th").val() === "x"){
+          alert("Shark Wins!");
+        }
+        else if ($(".1st").val() === "x" && $(".5th").val() === "x" && $(".9th").val() === "x"){
+          alert("Shark Wins!");
+        }
+        else if ($(".3rd").val() === "x" && $(".5th").val() === "x" && $(".7th").val() === "x"){
+          alert("Shark Wins!");
+        }
+        else if (playerTurn === 9){
+          alert("Draw!");
+        }
+    }
 
 
 
