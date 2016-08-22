69cfae637400b7bbebe0dc836346f9c1bbb1cdee
cabrown91 <cleo.aliciabrown@gmail.com>
2016-08-22 06:46:36 -0700

    add player change function

--- js/app.js
+++ js/app.js
@@ -3,4 +3,24 @@ $(document).ready(function() {
   // all code to manipulate the DOM
   // goes inside this function
 
+  var $box = $(".box");
+
+  var turn = "X";
+
+
+  function changePlayer() {
+  if (turn === "X") {
+    turn = "O";
+  } else {
+    turn = "X";
+  }
+}
+
+  $box.on("click", function handleClick() {
+      $(this).append(turn);
+      changePlayer();
+  });
+
+
+
 });
